c6b2f0e303fcfd1d701de329269d29971ad18dcb
KevinZZZZ1 <zuoyenana@qq.com>
2018-09-12 20:48:53 +0800

    #07 in leetcode

---
+++ leetcode/Num07.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Num07 {
+	
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+	
+	public static void main(String[] args) {
+		Num07 n = new Num07();
+		int a = 3425253;
+		System.out.println(n.reverse(a));
+	}
+
+}
